4b6d5efbc207768050a943a7a7f0d01e06e07382
Incomesite78 <145444524+Incomesite78@users.noreply.github.com>
2023-10-04 19:04:33 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,3 @@
+document.getElementById('pageButton').addEventListener('click', function() {
+    window.location.href = 'https://incomesite78.github.io/login'; // Replace 'page2.html' with the URL of the page you want to navigate to.
+  });
